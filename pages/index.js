c3efeebffcf529733e592823da5d8d74a389c923
Charlene <charlenelcapili@gmail.com>
2023-04-13 03:27:55 -0400

    added more query, markup fixes on blog card component

--- pages/index.js
+++ pages/index.js
@@ -9,21 +9,24 @@ const QUERY = gql`
   {
     posts {
       id
-      title
-      slug
-      featuredImage {
-        url
-      }
-      content {
-        html
-      }
-      date_published
       author {
         name
         avatar {
           url
         }
+        avatarAlt
       }
+      title
+      slug
+      featuredImage {
+        url
+      }
+      featuredImageAlt
+      excerpt
+      content {
+        html
+      }
+      date_published
     }
   }
 `;
@@ -40,14 +43,16 @@ export async function getStaticProps() {
 
 export default function Home({ posts }) {
   return (
-    <main>
+    <main className='grid max-w-[26rem] sm:max-w-[52.5rem] mt-16 sm:mt-20 md:mt-32 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mx-auto gap-2 lg:gap-y-4 xl:gap-x-4 lg:max-w-7xl px-3 lg:px-4 xl:px-0'>
       {posts.map((post) => (
         <BlogCard 
           key={post.id} 
+          author={post.author} 
           title={post.title} 
           slug={post.slug} 
-          author={post.author} 
           featuredImage={post.featuredImage} 
+          featuredImageAlt={post.featuredImageAlt}
+          excerpt={post.excerpt} 
           datePublished={post.date_published}
         />
       ))}
--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -2,28 +2,34 @@ import Link from 'next/link'
 import Image from 'next/image'
 import styles from '../styles/BlogCard.module.css'
 
-export default function BlogCard({title, slug, author, featuredImage, datePublished}) {
+export default function BlogCard({author, title, slug, featuredImage, featuredImageAlt, excerpt, datePublished}) {
+  // format date and time
+  let formatDate = new Date(datePublished);
+  let dateOptions = { month: 'long', day: 'numeric', year: 'numeric' };
+  let date = formatDate.toLocaleDateString('en-US', dateOptions);
+
   return(
     <div className={styles.card}>
       <Link href={'/posts/' + slug}>
         <div className={styles.imgContainer}>
-          <img src={featuredImage.url} alt='' />
+          <img src={featuredImage.url} alt={featuredImageAlt} />
+        </div>
+
+        <div className={styles.text}>
+          <h2>{title}</h2>
+          <p>{excerpt}</p>
+          <div className={styles.details}>
+            <div className={styles.author}>
+              <img src={author.avatar.url} alt={author.avatarAlt} />
+              <h3>{author.name}</h3>
+            </div>
+
+            <div>
+              <span>{date}</span>
+            </div>
+          </div>
         </div>
       </Link>
-
-      <div className={styles.text}>
-        <h2>{title}</h2>
-        <div className={styles.details}>
-          <div className={styles.author}>
-            <img src={author.avatar.url} alt='' />
-            <h3>{author.name}</h3>
-          </div>
-
-          <div className={styles.date}>
-            <h3 className='font-poppins'>{datePublished}</h3>
-          </div>
-        </div>
-      </div>
     </div>
   )
 }
