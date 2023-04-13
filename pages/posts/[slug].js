6c96de636654eb871662bc0ca760650bde9845ba
Charlene <charlenelcapili@gmail.com>
2023-04-13 11:03:36 -0400

    fixed markup issues

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -79,22 +79,27 @@ export async function getStaticProps({params}) {
 }
 
 export default function BlogArticle({post}) {
+  // format date and time
+  let formatDate = new Date(post.date_published);
+  let dateOptions = { month: 'long', day: 'numeric', year: 'numeric' };
+  let date = formatDate.toLocaleDateString('en-US', dateOptions);
+
   return (
     <main className='pt-8 pb-16 lg:pt-16 lg:pb-24'>
       <div className='flex justify-between px-4 mx-auto max-w-screen-xl'>
-        <article class="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue">
-          <header class="mb-4 lg:mb-6 not-format">
-            <address class="flex items-center mb-6 not-italic">
-                <div class="inline-flex items-center mr-3 text-sm">
-                    <img class="mr-4 w-16 h-16 rounded-full" src={post.author.avatar.url} alt={post.author.avatarAlt} />
+        <article className="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue">
+          <header className="mb-4 lg:mb-6 not-format">
+            <address className="flex items-center mb-6 not-italic">
+                <div className="inline-flex items-center mr-3 text-sm">
+                    <img className="mr-4 w-16 h-16 rounded-full" src={post.author.avatar.url} alt={post.author.avatarAlt} />
                     <div>
-                        <a href="#" rel="author" class="text-xl font-bold">{post.author.name}</a>
-                        <p class="text-base font-light text-gray-500">{post.author.title}</p>
-                        <p class="text-base font-light text-gray-500"><time pubdate datetime="2022-02-08" title="February 8th, 2022">Feb. 8, 2022</time></p>
+                        <a href="#" rel="author" className="text-xl font-bold">{post.author.name}</a>
+                        <p className="text-base font-light text-gray-500">{post.author.title}</p>
+                        <p className="text-base font-light text-gray-500"><time pubdate="true" dateTime="2022-02-08" title={date}>{date}</time></p>
                     </div>
                 </div>
             </address>
-            <h1 class="mb-4 text-3xl font-extrabold leading-tight lg:mb-6 lg:text-4xl">{post.title}</h1>
+            <h1 className="mb-4 text-3xl font-extrabold leading-tight lg:mb-6 lg:text-4xl">{post.title}</h1>
           </header>
 
           <div dangerouslySetInnerHTML={
