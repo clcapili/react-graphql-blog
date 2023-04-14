d79f0024ecd04046d175d2501bb64ee285f89062
Charlene <charlenelcapili@gmail.com>
2023-04-13 20:49:23 -0400

    removed unused module blog card

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -1,5 +1,4 @@
 import Link from 'next/link'
-import Image from 'next/image'
 import styles from '../styles/BlogCard.module.css'
 
 export default function BlogCard({author, title, slug, featuredImage, featuredImageAlt, excerpt, datePublished}) {
